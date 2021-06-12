9521a1c2b6ce994b4e387b3701b037171ba0201c
Aidan Pilgrim <aidan@Aidans-MacBook-Air.local>
2021-06-12 12:26:29 +0100

    completed form

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,3 +3,5 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+
+from flaskr import routes
--- tests/test_flaskr/test_routes.py
+++ tests/test_flaskr/test_routes.py
@@ -1,4 +1,39 @@
-import pytest
-
+"""
+This file (test_routes.py) contains the unit tests for the routes.py file.
+"""
+from werkzeug.wrappers import response
 from flaskr import app
 
+
+def test_index_page():
+    """
+    GIVEN a Flask application
+    WHEN the '/' page is requested (GET)
+    THEN check the response is valid
+    """
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+        assert b'<h1>Quick Search' in response.data
+        assert b'<li>set up some lookup' in response.data
+        assert b'<form method="post" action=""' in response.data
+        assert b'<input name="address1" type="text" required' in response.data
+        assert b'<input name="key" type="text" placeholder="key location" required' in response.data
+        assert b'<button type="submit' in response.data
+
+def test_post_index_page():
+    """
+    GIVEN a flask app
+    WHEN the '/ page is posted to 'POST`
+    THEN check that the data is collected in a py dict
+    """
+    with app.test_client() as client:
+        response = client.post('/',
+                                data={'address1': 'YO32 9GX',
+                                'address2': 'YO10 3LF',
+                                'address3': 'YO1 7HH',
+                                'address4': 'YO1 9TJ',
+                                'address5': 'YO24 1AB',
+                                'key': 'YO31 8HY'},
+                                follow_redirects=True)
+        assert response.status_code == 200
