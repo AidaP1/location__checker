37d7491bb1c7a59c266c97cb0ce8980c2f455075
Aidan Pilgrim <aidan@Aidans-MacBook-Air.local>
2021-06-14 15:41:10 +0100

    added tests for fewer locations. RED

--- app/query_gmaps.py
+++ app/query_gmaps.py
@@ -2,7 +2,35 @@ import requests
 import os
 
 
-def call_google(query):    
+def call_API(query):
+    try:
+        api_key = os.environ.get("API_KEY")
+        url = f"https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins={query['key']}&destinations={query['address1']}|{query['address2']}|{query['address3']}|{query['address4']}|{query['address5']}&key={api_key}"
+        response = requests.get(url)
+        trips = response.json()
+        response.raise_for_status()
+    except requests.RequestException:
+        return None
+    return trips
+
+def format_response(trips):
+    output = {
+    }
+    i = 1
+    
+    for trip in trips['rows'][0]['elements']:
+        key = 'address' + str(i)
+        value = trip['duration']['text']
+        output[key] = value
+        i += 1
+    
+    return output
+
+def call_google(query):
+    api_response = call_API(query)
+    return format_response(api_response)
+
+
     try:
         api_key = os.environ.get("API_KEY")
         url = f"https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins={query['key']}&destinations={query['address1']}|{query['address2']}|{query['address3']}|{query['address4']}|{query['address5']}&key={api_key}"
--- tests/test_flaskr/test_query_gmaps.py
+++ tests/test_flaskr/test_query_gmaps.py
@@ -3,13 +3,78 @@ This file (test_query_gmaps.py) contains the unit tests for the query_gmaps.py f
 """
 
 from werkzeug.wrappers import response
-from app.query_gmaps import call_google
+from app.query_gmaps import call_google, call_API, format_response
 
-def test_call_google_five_loc():
+def test_call_API_5_loc():
+    """
+    GIVEN a set of 5 locations and a key value
+    WHEN the google maps API is called
+    THEN receive a successful response
+    """
+    query = {'address1': 'YO32 9GX',
+            'address2': 'YO10 3LF',
+            'address3': 'YO1 7HH',
+            'address4': 'YO1 9TJ',
+            'address5': 'YO24 1AB',
+            'key': 'YO31 8HY'}
+    data = call_API(query)
+    assert data['status'] =='OK'
+
+def test_call_API_4_loc():
+    """
+    GIVEN a set of 4 locations and a key value
+    WHEN the google maps API is called
+    THEN receive a successful response
+    """
+    query = {'address1': 'YO32 9GX',
+            'address2': 'YO10 3LF',
+            'address3': 'YO1 7HH',
+            'address4': 'YO1 9TJ',
+            'key': 'YO31 8HY'}
+    data = call_API(query)
+    assert data['status'] =='OK'
+
+def test_call_API_3_loc():
+    """
+    GIVEN a set of 3 locations and a key value
+    WHEN the google maps API is called
+    THEN receive a successful response
+    """
+    query = {'address1': 'YO32 9GX',
+            'address2': 'YO10 3LF',
+            'address3': 'YO1 7HH',
+            'key': 'YO31 8HY'}
+    data = call_API(query)
+    assert data['status'] =='OK'
+
+def test_call_API_2_loc():
+    """
+    GIVEN a set of 2 locations and a key value
+    WHEN the google maps API is called
+    THEN receive a successful response
+    """
+    query = {'address1': 'YO32 9GX',
+            'address2': 'YO10 3LF',
+            'key': 'YO31 8HY'}
+    data = call_API(query)
+    assert data['status'] =='OK'
+
+def test_call_API_1_loc():
+    """
+    GIVEN a set of 1 locations and a key value
+    WHEN the google maps API is called
+    THEN receive a successful response
+    """
+    query = {'address1': 'YO32 9GX',
+            'key': 'YO31 8HY'}
+    data = call_API(query)
+    assert data['status'] =='OK'
+
+def test_call_google():
     """
     GIVEN a set of 5 addresses and a key location
     WHEN the google maps API is called
-    THEN check the response is valid
+    THEN check the response is formatted and can be used on-site
     """
     query = {'address1': 'YO32 9GX',
             'address2': 'YO10 3LF',
