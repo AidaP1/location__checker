b786f4cb026ba6238f8325bdbdf5a2581b23553d
Aidan Pilgrim <aidan@Aidans-MacBook-Air.local>
2021-06-16 20:42:56 +0100

    swap to a factory function in init - nothing works, yet

---
+++ tests/test_app/test_admin/test_admin.py
@@ -0,0 +1,10 @@
+from app.admin.admin import log_in, log_out
+
+def test_login_required():
+    assert True == False
+
+def test_log_in():
+    assert True == False
+
+def test_log_out():
+    assert True == False
