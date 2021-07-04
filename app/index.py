17600feac1ac2ac25e31a243106e6061330feaff
Aidan Pilgrim <aidan@Aidans-MacBook-Air.local>
2021-07-04 18:45:06 +0100

    location saving is now available

--- app/index.py
+++ app/index.py
@@ -38,21 +38,21 @@ def locations():
             error = 'You must enter a location name'
         elif not loc_adr:
             error = 'You must enter a location address'
-        elif db.execute('''SELECT id FROM user
+        elif db.execute('''SELECT user.id FROM user
                             JOIN location ON user.id = location.user_id
                             WHERE user.id = ? AND location.name = ?''', (user_id, loc_name,)).fetchone() is not None:
             error = 'You already have a location with that name'
-        elif db.execute('''SELECT id FROM user
+        elif db.execute('''SELECT user.id FROM user
                             JOIN location ON user.id = location.user_id
                             WHERE user.id = ? AND location.address = ?''', (user_id, loc_adr,)).fetchone() is not None:
             error = 'You already have that location saved'
         
         if error == None:
-            db.execute('INSERT INTO location (user_id, location.name, location.address) VALUES (?, ?, ?)', (user_id, loc_name, loc_adr,))
+            db.execute('INSERT INTO location (user_id, name, address) VALUES (?, ?, ?)', (user_id, loc_name, loc_adr,))
             db.commit()
     
     
-    locations = db.execute('''SELECT user.id, location_name, location_address FROM user
+    locations = db.execute('''SELECT user.id, name, address FROM user
                                 JOIN location ON user.id = location.user_id
                                 WHERE user.id = ?''', (user_id,)).fetchall()
     return render_template('locations.html', locations=locations)
